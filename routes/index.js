afa14f66fd6019e099140d0dc0f9ffd59dac6d3f
Mani <m.tahriri.z@gmail.com>
2020-03-24 15:40:10 +0100

    index.js in routs folder modified.. two static routs added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+// top ejs file
+router.get('/top' , (req, res, next) => {
+  res.render('top');
+});
+
+// main ejs file
+router.get('/main' , (req, res, next) => {
+  res.render('main');
+});
+
+// 
+
 module.exports = router;
