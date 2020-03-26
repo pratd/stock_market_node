70aa858143a7102a579d7e7c6e018dabe45ae602
prattya datta <pratt@LAPTOP-500LE5P5.(none)>
2020-03-26 16:28:19 +0100

    no changes

--- models/index.js
+++ models/index.js
@@ -4,8 +4,8 @@ module.exports = {
 	index: function(url){
 		return new Promise((resolve, reject) => {
 			request(url, { json: true }, (err, res, body) => {
-				if (err) reject(err)
-				resolve(body)
+				if (err) reject(err);
+				resolve(body);
 			});
 		})
 	}
